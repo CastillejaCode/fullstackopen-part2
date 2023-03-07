e8eab7b50b5f4aebf1155430f98f320f9b0e56ba
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-03-06 19:16:22 -0800

    Finish weather portion

--- countries/src/components/Countries.jsx
+++ countries/src/components/Countries.jsx
@@ -1,33 +1,53 @@
-const Country = ({ array }) => {
-	const country = array[0];
-	return (
-		<div>
-			<h1>{country.name.common}</h1>
-			<h2>Capital: {country.capital}</h2>
-			<h2>Area: {country.area} km2</h2>
-			<ul>
-				{Object.entries(country.languages).map((key) => (
-					<li key={key[0]}>{key[1]}</li>
-				))}
-			</ul>
-			<h1>{country.flag}</h1>
-		</div>
-	);
+import { useState, useEffect } from 'react';
+import backendService from '../services/backend';
+
+const Country = ({ country }) => {
+	const [weather, setWeather] = useState(null);
+
+	useEffect(() => {
+		backendService.getWeather(country.capital).then((data) => {
+			setWeather(data);
+			console.log(data);
+		});
+	}, []);
+
+	if (!weather) null;
+	else {
+		return (
+			<div>
+				<h1>{country.name.common}</h1>
+				<h2>Capital: {country.capital}</h2>
+				<h2>Area: {country.area} km2</h2>
+				<h2>Languages</h2>
+				<ul>
+					{Object.entries(country.languages).map((key) => (
+						<li key={key[0]}>{key[1]}</li>
+					))}
+				</ul>
+				<h1>{country.flag}</h1>
+				<h1>Weather in {country.capital}</h1>
+				<h2>Temp: {weather.main.temp} C</h2>
+				<img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather conditions' />
+				<h2>Wind: {weather.wind.speed} m/s</h2>
+			</div>
+		);
+	}
 };
 
 const Countries = ({ array }) => {
 	if (array.length > 10) return <h2>Too many matches, specify another filter</h2>;
-	else if (array.length === 1) return <Country array={array} />;
+	else if (array.length === 1) return <Country country={array[0]} />;
 	else {
 		return (
 			<ul>
-				{array.map((country) => (
-					<div>
+				{array.map((country) => {
+					return (
 						<li key={country.name.common}>
-							{country.name.common} <button> Show More</button>
+							{country.name.common}
+							<button>Show More</button>
 						</li>
-					</div>
-				))}
+					);
+				})}
 			</ul>
 		);
 	}
--- countries/src/services/backend.js
+++ countries/src/services/backend.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 const baseURL = `https://restcountries.com/v3.1/all`;
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?`;
+const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+const api = import.meta.env.VITE_API;
+console.log(typeof api);
 
-const getQuery = (country) => {
+const getQuery = () => {
+	console.log('api', api);
+	console.log('url', `${geoURL}Bern&limit=1&appid=${api}`);
+
 	return axios.get(baseURL).then((response) => response.data);
+};
+
+const getWeather = async (capital) => {
+	const place = await axios.get(`${geoURL}${capital}&limit=1&appid=${api}`).then((response) => response.data);
+	const [lat, lon] = [place[0].lat, place[0].lon];
+
+	const weather = await axios
+		.get(`${weatherURL}lat=${lat}&lon=${lon}&units=imperial&appid=${api}`)
+		.then((response) => response.data);
+	return weather;
 };
 
 export default {
 	getQuery,
+	getWeather,
 };
